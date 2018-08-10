3f712f66d000fa02883093ae60648d2f7b1bc847
vyroj <leepong.amerman@gmail.com>
2018-08-10 00:35:50 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+var data = {
+
+  "test": "testo",
+
+  "members" : {
+    "allieamerman" : {
+      "name" : "Allie Amerman",
+      "pfp" : "media/allie.png",
+      "desc" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+      "teams" : "Team 1",
+      "games" : ["Color Tunnel"]
+    }
+  },
+
+  "teams" : {
+    "team1" : {
+      "name" : "Team 1",
+      "pfp" : "",
+      "desc" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid",
+      "currentmembers" : ["Allie Amerman"],
+      "pastmembers" : [""],
+      "games" : ["Color Tunnel"]
+    }
+  },
+
+  "games" : {
+    "colortunnel" : {
+      "name" : "Color Tunnel",
+      "pictures" : ["media/img1.jpg","media/img2.jpg"],
+      "videos" : ["media/vid3.mp4"],
+      "desc" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed dostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      "download" : {
+        "windows" : "down",
+        "osx" : "down",
+        "linux" : "down"
+      },
+      "creators" : [
+        {"name" : "allieamerman", "role" : "code"},
+        {"name" : "allieamerman", "role" : "code2"}
+      ]
+    }
+  }
+}
